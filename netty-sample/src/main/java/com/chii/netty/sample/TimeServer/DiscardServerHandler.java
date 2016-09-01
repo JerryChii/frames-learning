4c7c4d9f65e6fbf0fbf519ca05fe1537e7716a4d
xujialin <1625742718@qq.com>
2016-09-01 23:54:31 +0800

    master:添加部分注释

--- netty-sample/src/main/java/com/chii/netty/sample/TimeServer/DiscardServerHandler.java
+++ netty-sample/src/main/java/com/chii/netty/sample/TimeServer/DiscardServerHandler.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Describe:
@@ -12,17 +13,28 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * Date:    2016/9/1
  */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+	/**
+	 * Please keep in mind that it is the handler's responsibility to release any reference-counted object passed to the handler.
+	 * @param ctx
+	 * @param msg
+	 * @throws Exception
+	 */
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf buf = (ByteBuf) msg;
-		byte[] req = new byte[buf.readableBytes()];
-		buf.readBytes(req);
-		String body = new String(req, "UTF-8");
-		System.out.println("request: " + body);
-		String currentTime = "QUERY TIMESTAMP".equalsIgnoreCase(body) ? System.currentTimeMillis() + "" : "BAD ORDER";
+		try {
+			ByteBuf buf = (ByteBuf) msg;
+			byte[] req = new byte[buf.readableBytes()];
+			buf.readBytes(req);
+			String body = new String(req, "UTF-8");
+			System.out.println("request: " + body);
+			String currentTime = "QUERY TIMESTAMP".equalsIgnoreCase(body) ? System.currentTimeMillis() + "" : "BAD ORDER";
 
-		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-		ctx.write(resp);
+			ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+			ctx.write(resp);
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
 	}
 
 	@Override
@@ -31,6 +43,12 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 		ctx.flush();
 	}
 
+	/**
+	 * the caught exception should be logged and its associated channel should be closed here
+	 * @param ctx
+	 * @param cause
+	 * @throws Exception
+	 */
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		ctx.close();
